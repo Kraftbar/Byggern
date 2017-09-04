8d47a15a195ac97e4575bcc8cbe2f9080ddb86ae
unknown <gautevn@itk-d654.win.ntnu.no>
2017-09-04 10:43:22 +0200

    Opprettet et prosjekt i Atmel studio 6.2

---
+++ Byggern/Byggern/Byggern.c
@@ -0,0 +1,17 @@
+/*
+ * Byggern.c
+ *
+ * Created: 04.09.2017 10:24:49
+ *  Author: gautevn
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
